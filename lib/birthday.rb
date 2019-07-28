667f2aed3116ef2d133a7d09451cc1dd9c8c12c3
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-28 22:26:40 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,9 +3,26 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
+def select_winner(passengers)
+  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite,name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
+end
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+  def age_appropriate_birthday(birthday_kids)
+    if birthday_kids < 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
 end
 
 
